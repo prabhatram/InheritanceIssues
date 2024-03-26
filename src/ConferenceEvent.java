0180b20c92822ed42444505a0dce8fbfd18264c6
HeYujieii <x00217230102@njit.edu.cn>
2024-03-26 21:24:26 +0800

    inheritancelssues

--- src/ConferenceEvent.java
+++ src/ConferenceEvent.java
@@ -1,4 +1,5 @@
-public class ConferenceEvent extends Event{
+public class ConferenceEvent{
+    
     private boolean breakfastRequired;
     private double breakfastCost;
     private boolean lunchRequired;
@@ -6,53 +7,74 @@ public class ConferenceEvent extends Event{
     private boolean dinnerRequired;
     private double dinnerCost;
     private double conferenceEventCost;
+    private String eventID;
+    private String eventName;
+    private String eventLocation;
+    private String pointOfContact;
+    private CalculateEventCostClass calculateEventCostObject;
+    private int totalParticipants;
+    private int totalEventDays;
+ 
+    public ConferenceEvent(String eventID, String eventName, String eventLocation, String pointOfContact, int totalParticipants, int totalEventDays,
+     double breakfastCost, double lunchCost, double dinnerCost) {
 
-    public double getBreakfastCost(){
-        return breakfastCost;
-    }
+        this.calculateEventCostObject = new CalculateEventCostClass();
 
-    public void setBreakfastCost(double breakfastCost){
-        this.breakfastCost = breakfastCost;
-    }
-
-    public double getLunchCost(){
-        return lunchCost;
-    }
-
-    public void setLunchCost(double lunchCost){
-        this.lunchCost = lunchCost;
-    }
-
-    public double getDinnerCost(){
-        return dinnerCost;
-    }
-
-    public ConferenceEvent(String eventID, String eventName, String eventLocation, String pointOfContact, int totalParticipants, int totalEventDays, 
-    double breakfastCost, double lunchCost, double dinnerCost){
-        super(eventID, eventName, eventLocation, pointOfContact, totalParticipants, totalEventDays);
+        this.eventID = eventID;
+        this.eventName = eventName;
+        this.eventLocation = eventLocation;
+        this.pointOfContact = pointOfContact;
         this.breakfastCost = breakfastCost;
         this.lunchCost = lunchCost;
         this.dinnerCost = dinnerCost;
-    }
+        this.totalParticipants = totalParticipants;
+        this.totalEventDays = totalEventDays;
+     }
 
+     public double getBreakfastCost() {
+         return breakfastCost;
+     }
 
-    @Override
-    public void calculateEventCost(){
-        super.calculateEventCost();
-        conferenceEventCost = getEventCost() + ((breakfastCost + lunchCost + dinnerCost) * getTotalParticipants() * getTotalEventDays());
-    }
+     public void setBreakfastCost(double breakfastCost) {
+         this.breakfastCost = breakfastCost;
+     }
 
+     public double getLunchCost() {
+         return lunchCost;
+     }
 
+     public void setLunchCost(double lunchCost) {
+         this.lunchCost = lunchCost;
+     }
+
+     public double getDinnerCost() {
+         return dinnerCost;
+     }
+
+     public void setDinnerCost(double dinnerCost) {
+         this.dinnerCost = dinnerCost;
+     }
+
+     public double getConferenceEventCost() {
+         return conferenceEventCost;
+     }
+
+     public void setConferenceEventCost(double conferenceEventCost) {
+         this.conferenceEventCost = conferenceEventCost;
+     }
+
+     
+     public void calculateEventCost() {
+       conferenceEventCost = calculateEventCostObject.calculateEventCost() + ((breakfastCost + lunchCost + dinnerCost) * totalParticipants * totalEventDays);
+     }
+   
     @Override
     public String toString(){
         return "Conference Event details: " + "\n" +
-        "Event ID: " + getEventID() + "\n" +
-        "Event Name: " + getEventName() + "\n" +
-        "Event Location: " + getEventLocation() + "\n" +
-        "Total participants: " + getTotalParticipants() + "\n" +
+        "Event ID: " + eventID + "\n" +
+        "Event Name: " + eventName + "\n" +
+        "Event Location: " + eventLocation + "\n" +    
+        "Total Participants: " + totalParticipants + "\n" +
         "Total Conference Cost: " + conferenceEventCost;
     } 
-        
 }
-
-    
